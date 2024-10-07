86765040a854e0af635eeae5bf075623991ffb89
Julian Fong <103627958+gragbag@users.noreply.github.com>
2024-10-06 22:41:58 -0700

    Creates new book on submit
    Added the javascript to create a new book object when info is submitted through the dialog.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const addBookButton = document.querySelector(".add-book");
 const dialog = document.querySelector("#dialog")
+const submitBook = document.querySelector(".submit-book");
+const addForm = document.querySelector("#add-form");
 
 
 const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
 }
 
 function addBookToLibrary() {
-	dialog.showModal();
+	const title = document.querySelector("#title").value;
+	const author = document.querySelector("#author").value || "N/A";
+	const pages = document.querySelector("#pages").value;
+	const read = document.querySelector("#read").value;
+
+	const newBook = new Book(title, author, pages, read);
+	
 }
 
+addBookButton.addEventListener("click", () => {
+	dialog.showModal();
+});
 
-addBookButton.addEventListener("click", addBookToLibrary);
+submitBook.addEventListener("click", (e) => {
+	e.preventDefault();
+	dialog.close();
+	addBookToLibrary();
+})
