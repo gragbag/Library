7a6d5cb61625918cbc90fb937d20a3a2efef5b60
Julian Fong <103627958+gragbag@users.noreply.github.com>
2024-10-06 22:04:12 -0700

    Added dialog modal to enter book info
    Made the add book button open a dialog modal for the user to input info about the book and have it added to the library.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const addBookButton = document.querySelector(".add-book");
+const dialog = document.querySelector("#dialog")
+
+
 const myLibrary = [];
 
 function Book() {
@@ -5,5 +9,8 @@ function Book() {
 }
 
 function addBookToLibrary() {
-	
+	dialog.showModal();
 }
+
+
+addBookButton.addEventListener("click", addBookToLibrary);
